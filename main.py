0d34e9a51cf9d1c509b9db9c4e84a39888be8ffa
Hunter Kinder <markhunterkinder@gmail.com>
2017-02-09 19:50:31 -0600

    finished user-signup

--- main.py
+++ main.py
@@ -15,24 +15,108 @@
 # limitations under the License.
 #
 import webapp2
-import jinja2
+import re
 
-from google.appengine.ext import db
+def buildwelcome(username):
+	welcome = """
+	<head>
+		<title>Unit 2 Signup</title>
+	</head>
+	<body>
+		<h2>Welcome """ + username + """!  </h2>
+	</body>"""
+	return welcome
 
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
-    
-def render_str(template, **params):
-	t = jinja_env.get_template(template)
-	return t.render(params)
+def buildpage(userError, passError, verifyError, emailError):
+	str_form = """
+		<form method = "post">
+		<table>
+		<tr>
+			<td class = "label">
+				Username
+			</td>
+			<td>
+				<input type="text" name="username">
+			</td>
+			<td class="error">
+			 """+userError +"""
+			</td>
+		</tr>
+		
+		<tr>
+			<td class="label">
+				Password
+			</td>
+			<td>
+				<input type="password" name="password">
+			</td>
+			<td class="error">"""+passError+"""
+			</td>
+		</tr>
+		
+		<tr>
+			<td class="label">
+				Verify Password
+			</td>
+			<td>
+				<input type="password" name="verify">
+			</td>
+			<td class="error">"""+verifyError+"""
+			</td>
+		</tr>	
+		<tr>
+			<td class="label">
+				Email (optional)
+			</td>
+			<td>
+				<input type="text" name="email">
+			</td>
+			<td class="error">"""+emailError+"""
+			</td>
+		</tr>
+		</table>
+		
+		<input type="submit">
+	</form>
+	"""
+	return str_form
 	
-class BaseHandler(webapp2.RequestHandler):
-	def render(self, template, **kw):
-		self.response.out.write(render_str(template, *kw))
+class MainHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.write(buildpage("","","",""))
+		
+	def post(self):
+		have_error = False
+		username = self.request.get("username")
+		password = self.request.get("password")
+		verify = self.request.get("verify")
+		email = self.request.get("email")
+		
+		params = dict(username="", email="",verify="", password="")
+		
+		if not valid_username(username):
+			params["username"] = "That's not a valid username."
+			have_error = True
+			
+		if not valid_password(password):
+			params["password"] = "That's not a valid password."
+			have_error = True
+			
+		if password != verify:
+			params["verify"] = "You passwords didn't match."
+			have_error = True
+		
+		if not valid_email(email):
+			params["email"] = "That's not a valid email."
+			have_error = True
+		
+			
+		if have_error:
+			self.response.write(buildpage(params["username"], params["password"], params["verify"], params["email"]))
+		else:
+			self.response.write(buildwelcome(username))				
+
 	
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
-    
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
 	return username and USER_RE.match(username)
@@ -45,46 +129,16 @@ EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
 	return not email or EMAIL_RE.match(email)
     
-class Signup(BaseHandler):
+class Welcome(MainHandler):
 	def get(self):
-		self.render("signup-form.html")
-		
-	def post(self):
-		have_error = False
-		username = self.request.get('username')
-		password = self.request.get('password')
-		verify = self.request.get('verify')
-		email = self.request.get('email')
-		
-		params = dict(username = username, email = email)
-		
-		if not valid_username(username):
-			params['error_username'] = "That's not a valid username."
-			have_error = True
-			
-		elif password != verify:
-			params['error_verify'] = "You passwords didn't match."
-			have_error = True
-		
-		if not valid_email(email):
-			params['error_email'] = "That's not a valid email."
-			have_error = True
-			
-		if have_error:
-			self.render('signup-form.html', **params)
-		else:
-			self.redirect('/unit2/welcome?username=' + username)				
-
-class Welcome(BaseHandler):
-	def get(self):
-		username = self.request.get('username')
+		username = self.request.get("username")
 		if valid_username(username):
-			self.render('welcome.html', username = username)
+			self.response.write(buildwelcome(username))
 		else:
 			self.redirect('/unit2/signup')
         
 
 app = webapp2.WSGIApplication([
-    ('/unit2/signup', Signup),
-    ('/unit2/welcome', Welcome)
+    ('/', MainHandler)
 ], debug=True)
+
